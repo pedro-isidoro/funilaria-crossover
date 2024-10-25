1d76fc404627e0c084e976452b787e8bd723c1d1
Pedro <pedro.isidoro26@yahoo.com.br>
2024-10-25 16:05:50 -0300

    feat ariaLabel to a tag

--- src/components/Employers/Employers.tsx
+++ src/components/Employers/Employers.tsx
@@ -9,8 +9,8 @@ export const Employers = () => {
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, distinctio. Molestias eaque sunt distinctio omnis maiores, dolores quis, ipsa quam placeat nisi excepturi quibusdam aspernatur iure commodi facere error nulla?</p>
           </div>
           <ul className="employer_card_nets">
-            <li className="employer_card_net_item"><a href="https://www.instagram.com/crossoverautos/" target="_blank" className="instagram Link"><FaInstagram className="insta"/></a></li>
-            <li className="employer_card_net_item"><a href="https://api.whatsapp.com/send?phone=5511976316322&text=Olá%20Cross%20Over%20-%20Funilaria%20Express%20%20e%20Estética%20Automotiva%2C%20eu%20preciso%20de%20mais%20informações%20sobre%3A" target="_blank" className="Whats Link"><FaWhatsapp className="whats"/></a></li>
+            <li className="employer_card_net_item"><a href="https://www.instagram.com/crossoverautos/" target="_blank" className="instagram Link"><FaInstagram className="insta" aria-label="Folow us in our social media"/></a></li>
+            <li className="employer_card_net_item"><a href="https://api.whatsapp.com/send?phone=5511976316322&text=Olá%20Cross%20Over%20-%20Funilaria%20Express%20%20e%20Estética%20Automotiva%2C%20eu%20preciso%20de%20mais%20informações%20sobre%3A" target="_blank" className="Whats Link" aria-label="Send us a message about your problem"><FaWhatsapp className="whats"/></a></li>
           </ul>
         </div>
         <div className="employer_card">
@@ -20,8 +20,8 @@ export const Employers = () => {
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, distinctio. Molestias eaque sunt distinctio omnis maiores, dolores quis, ipsa quam placeat nisi excepturi quibusdam aspernatur iure commodi facere error nulla?</p>
           </div>
           <ul className="employer_card_nets">
-            <li className="employer_card_net_item"><a href="https://www.instagram.com/crossoverautos/" target="_blank" className="instagram Link"><FaInstagram className="insta"/></a></li>
-            <li className="employer_card_net_item"><a href="https://api.whatsapp.com/send?phone=5511976316322&text=Olá%20Cross%20Over%20-%20Funilaria%20Express%20%20e%20Estética%20Automotiva%2C%20eu%20preciso%20de%20mais%20informações%20sobre%3A" target="_blank" className="Whats Link"><FaWhatsapp className="whats"/></a></li>
+            <li className="employer_card_net_item"><a href="https://www.instagram.com/crossoverautos/" target="_blank" className="instagram Link" aria-label="Folow us in our social media"><FaInstagram className="insta" /></a></li>
+            <li className="employer_card_net_item"><a href="https://api.whatsapp.com/send?phone=5511976316322&text=Olá%20Cross%20Over%20-%20Funilaria%20Express%20%20e%20Estética%20Automotiva%2C%20eu%20preciso%20de%20mais%20informações%20sobre%3A" target="_blank" className="Whats Link" aria-label="Send us a message about your problem"><FaWhatsapp className="whats"/></a></li>
           </ul>
         </div>
       </section>
--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -17,8 +17,8 @@ export const Footer = () => {
         </nav>
         <div className="footer_second_section">
           <ul className="nets">
-            <li className="net_item"><a href="https://www.instagram.com/crossoverautos/" target="_blank" className="instagram Link"><FaInstagram className="insta"/></a></li>
-            <li className="net_item"><a href="https://api.whatsapp.com/send?phone=5511976316322&text=Olá%20Cross%20Over%20-%20Funilaria%20Express%20%20e%20Estética%20Automotiva%2C%20eu%20preciso%20de%20mais%20informações%20sobre%3A" target="_blank" className="whtatsApp Link"><FaWhatsapp className="whats"/></a></li>
+            <li className="net_item"><a href="https://www.instagram.com/crossoverautos/" target="_blank" className="instagram Link" aria-label="Folow us in our social media"><FaInstagram className="insta"/></a></li>
+            <li className="net_item"><a href="https://api.whatsapp.com/send?phone=5511976316322&text=Olá%20Cross%20Over%20-%20Funilaria%20Express%20%20e%20Estética%20Automotiva%2C%20eu%20preciso%20de%20mais%20informações%20sobre%3A" target="_blank" className="whtatsApp Link" aria-label="Send us a message about your problem"><FaWhatsapp className="whats"/></a></li>
           </ul>
         </div>
       </section>
