28dd4a1ee8eb77b1813ffd1ad4b730c0aebef527
Pedro <pedro.isidoro26@yahoo.com.br>
2024-12-14 09:29:19 -0300

    feat

--- src/components/Employers/Employers.tsx
+++ src/components/Employers/Employers.tsx
@@ -28,10 +28,10 @@ export const Employers = () => {
     return(
         <section className="section_employers">
         <div className="employer_card first_card">
-          <img src="/images/donos/Foto-minha.webp" alt="Employer Image 1" />
+          <img src="/images/team/odilon-2.webp" alt="Employer Image 1" />
           <div className="employer_card_texts">
-            <h2>Sócio 1</h2>
-            <p>Compromisso com Nossos Clientes!<br/><br/>Entendemos que o dia a dia é agitado e que o tempo é precioso. Por isso, nossa equipe é dedicada a fornecer serviços rápidos e de alta qualidade.<br/><br/>Temos recebido muitos elogios e feedbacks positivos de nossos clientes. Confira alguns depoimentos e áudios abaixo.</p>
+            <h2>Odilon</h2>
+            <p>Compromisso com Nossos Clientes!<br/><br/>Entendemos que o dia a dia é agitado e que o tempo é precioso. Por isso, nossa equipe é dedicada a fornecer serviços rápidos e de alta qualidade.<br/><br/>Temos recebido muitos elogios e feedbacks positivos de nossos clientes. Confira alguns depoimentos abaixo.</p>
           </div>
           <ul className="employer_card_nets">
             <li className="employer_card_net_item"><a href="https://www.instagram.com/crossoverautos/" target="_blank" className="instagram Link"><FaInstagram className="insta" aria-label="Folow us in our social media"/></a></li>
@@ -39,10 +39,10 @@ export const Employers = () => {
           </ul>
         </div>
         <div className="employer_card second_card">
-          <img src="/images/donos/Foto-minha.webp" alt="Employer Image 2" />
+          <img src="/images/team/zeca-2.webp" alt="Employer Image 2" />
           <div className="employer_card_texts">
-            <h2>Sócio 2</h2>
-            <p>Somos comprometidos com nossos clientes.<br/><br/>Entendemos que nos dias de hoje cada vez mais corrido e falta de tempo, montamos uma equipe comprometida com de entregar nossos trabalhos RÁPIDO E COM QUALIDADE.<br/><br/>E vem dando muito certo, recebemos muitos Feedbacks e elogios de nossos clientes. Veja a baixo alguns áudios e depoimentos</p>
+            <h2>Zeca</h2>
+            <p>Somos comprometidos com nossos clientes.<br/><br/>Entendemos que nos dias de hoje cada vez mais corrido e falta de tempo, montamos uma equipe comprometida com de entregar nossos trabalhos rápido e com qualidade.<br/><br/>E vem dando muito certo, recebemos muitos Feedbacks e elogios de nossos clientes. Veja a baixo alguns áudios e depoimentos</p>
           </div>
           <ul className="employer_card_nets">
             <li className="employer_card_net_item"><a href="https://www.instagram.com/crossoverautos/" target="_blank" className="instagram Link" aria-label="Folow us in our social media"><FaInstagram className="insta" /></a></li>
