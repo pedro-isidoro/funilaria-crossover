8200f2a61f2ad8d103acd6795419ac5132a908bd
Pedro <pedro.isidoro26@yahoo.com.br>
2024-12-14 11:36:29 -0300

    code

--- src/components/Employers/Employers.tsx
+++ src/components/Employers/Employers.tsx
@@ -35,7 +35,7 @@ export const Employers = () => {
           </div>
           <ul className="employer_card_nets">
             <li className="employer_card_net_item"><a href="https://www.instagram.com/crossoverautos/" target="_blank" className="instagram Link"><FaInstagram className="insta" aria-label="Folow us in our social media"/></a></li>
-            <li className="employer_card_net_item"><a href="https://api.whatsapp.com/send?phone=5511976316322&text=Olá%20Cross%20Over%20-%20Funilaria%20Express%20%20e%20Estética%20Automotiva%2C%20eu%20preciso%20de%20mais%20informações%20sobre%3A" target="_blank" className="Whats Link" aria-label="Send us a message about your problem"><FaWhatsapp className="whats"/></a></li>
+            <li className="employer_card_net_item"><a href="https://api.whatsapp.com/send?phone=5511997863738&text=Olá%20Cross%20Over%20-%20Funilaria%20Express%20%20e%20Estética%20Automotiva%2C%20eu%20preciso%20de%20mais%20informações%20sobre%3A" target="_blank" className="Whats Link" aria-label="Send us a message about your problem"><FaWhatsapp className="whats"/></a></li>
           </ul>
         </div>
         <div className="employer_card second_card">
